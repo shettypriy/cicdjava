640feeaf1c961aeb69d324ee400012d1ad920a5a
shettypriy <pshetty215@gmail.com>
2018-10-03 08:00:31 +0000

    2 files added

---
+++ src/main/java/mvn_pro/App.java
@@ -0,0 +1,13 @@
+package mvn_pro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
